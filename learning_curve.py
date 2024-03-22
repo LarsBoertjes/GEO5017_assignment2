32e4e114be28691adf8b095fd811d702e226ee42
jsscmnhn <J.A.Monahan@student.tudelft.nl>
2024-03-22 17:50:54 +0100

    learning curve should be done
    can still experiment with parameters

--- learning_curve.py
+++ learning_curve.py
@@ -13,9 +13,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from extracting_features import feature_extraction, data_loading
 from sklearn.model_selection import LearningCurveDisplay, ShuffleSplit
+from sklearn.preprocessing import StandardScaler
 
 
-def learning_curve(X, y, classifier, check_interval=0.05, num_experiments=10):
+def learning_curve(X, y, classifier, check_interval=0.1, num_experiments=50):
     # Range of training set sizes
     training_ratios = np.arange(check_interval, 1.0, check_interval)
 
@@ -28,61 +29,79 @@ def learning_curve(X, y, classifier, check_interval=0.05, num_experiments=10):
     for train_size in training_ratios:
         test_size = 1.0 - train_size
         training_examples.append(int(len(X) * train_size))
+        scaler = StandardScaler()
+
 
         # Lists to store errors for each experiment
         apparent_errors_exp = []
         true_errors_exp = []
 
-
         for _ in range(num_experiments):
             # Data splitting
             X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_size, test_size=test_size)
+            # X_train = scaler.fit_transform(X_train)
+            # X_test = scaler.transform(X_test)
 
             # Training & predicting
             classifier.fit(X_train, y_train)
             train_pred = classifier.predict(X_train)
             test_pred = classifier.predict(X_test)
 
-            # Apperent error calculation
+            # Apparent error calculation
             apparent_error = 1 - accuracy_score(y_train, train_pred)
             apparent_errors_exp.append(apparent_error)
 
-            # True error calculation SVM
+            # True error calculation
             true_error = 1 - accuracy_score(y_test, test_pred)
             true_errors_exp.append(true_error)
 
-        # Calculate average errors for this training set size
-        avg_apparent_error = np.mean(apparent_errors_exp)
-        avg_true_error = np.mean(true_errors_exp)
-
-        # Store average errors
-        svm_apparent_errors.append(avg_apparent_error)
-        svm_true_errors.append(avg_true_error)
+        # Store errors for this training set size
+        apparent_errors.append(apparent_errors_exp)
+        true_errors.append(true_errors_exp)
 
     return training_examples, apparent_errors, true_errors
 
-def plot_learning_curve(classifier, training_examples, apparent_errors, true_errors):
+def plot_learning_curve(classifier_name, training_examples, apparent_errors, true_errors):
+    ''' Plots the learning curve of a classifier.
+            classifier_name: Name of the classifier (string)
+            training_examples: Amount of training examples for each tested interval
+            apparent_errors: training set errors of all experiments
+            true_errors: approximated true error rate from the testing set'''
     plt.figure(figsize=(10, 6))
-    plt.plot(training_examples, svm_apparent_errors, label='Apparent Error', marker='o')
-    plt.plot(training_examples, svm_true_errors, label='True Error', marker='o')
+
+    # Calculate min and max errors
+    min_apparent_errors = [np.min(errors) for errors in apparent_errors]
+    max_apparent_errors = [np.max(errors) for errors in apparent_errors]
+    min_true_errors = [np.min(errors) for errors in true_errors]
+    max_true_errors = [np.max(errors) for errors in true_errors]
+
+    # Plotting the filled areas
+    plt.fill_between(training_examples, min_apparent_errors, max_apparent_errors, color='orange', alpha=0.1, label='Apparent Error Range')
+    plt.fill_between(training_examples, min_true_errors, max_true_errors, color='blue', alpha=0.1, label='True Error Range')
+
+    # Plotting average values
+    avg_apparent_errors = [np.mean(errors) for errors in apparent_errors]
+    avg_true_errors = [np.mean(errors) for errors in true_errors]
+    plt.plot(training_examples, avg_apparent_errors, label='Average Apparent Error', color='orange', marker='o')
+    plt.plot(training_examples, avg_true_errors, label='Average True Error', color='blue', marker='o')
+
     plt.xlabel('Training Set Size')
-    plt.ylabel('Classification Error')
-    plt.title(f'{classifier} Learning Curve')
+    plt.ylabel('Overal Accuracy Score')
+    plt.title(f'{classifier_name} Learning Curve')
     plt.legend()
     plt.grid(True)
     plt.show()
 
 
-# Plotting function for Random Forest learning curve
 
-ID, X_all, y = data_loading()
-X = X_all[:, [2, 5, 6, 7]]
-
-lin = svm.SVC(kernel='linear', C=1)
-lin2 = svm.SVC(kernel='linear', C=10)
-
-training_sizes, svm_apparent_errors, svm_true_errors = learning_curve(X, y, lin)
-plot_learning_curve('SVM', training_sizes, svm_apparent_errors, svm_true_errors)
+# ID, X_all, y = data_loading()
+# X = X_all[:, [2, 5, 6, 7]]
+#
+# lin = svm.SVC(kernel='linear', C=1)
+# lin2 = svm.SVC(kernel='linear', C=100)
+#
+# training_sizes, svm_apparent_errors, svm_true_errors = learning_curve(X, y, lin2)
+# plot_learning_curve('SVM', training_sizes, svm_apparent_errors, svm_true_errors)
 
 
 
